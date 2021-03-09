9a3ee9b0c84c0c3757db0eaeacbfafc5da5d4e31
Dirk Wright the 2nd <dirkwrightthe2nd@MacBook-Air-5.local>
2021-03-09 13:12:42 -0800

    commit

---
+++ .vscode/cquery_cached_index/@Users@dirkwrightthe2nd@Documents@RoboCarTeleOp_2/src@opcontrol.c
@@ -0,0 +1,13 @@
+
+
+#include "main.h"
+
+// Mr Wright was here
+//I dont know if im doing this correctly. -Reuben
+//wats up from Nate 
+void operatorControl() {
+	while (1) {
+		printf("Hello PROS User!\n");
+		delay(20);
+	}
+}
