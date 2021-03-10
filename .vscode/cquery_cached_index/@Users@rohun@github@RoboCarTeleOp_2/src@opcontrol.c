d0b5b681d55d194f5dec6560db520ad0a5889448
Rohun Barot <rohun@Rohuns-Mac-mini.attlocal.net>
2021-03-10 13:17:56 -0800

    name 2

--- .vscode/cquery_cached_index/@Users@rohun@github@RoboCarTeleOp_2/src@opcontrol.c
+++ .vscode/cquery_cached_index/@Users@rohun@github@RoboCarTeleOp_2/src@opcontrol.c
@@ -3,15 +3,15 @@
 // Mr Wright was here
 //I dont know if im doing this correctly. -Reuben
 //wats up from Nate
-// yo i'm here - rohun
+// Rohun 
 //This is just a test. You will probably have to change the port numbers.
 void operatorControl() {
 	while (1) {
 		delay(10);
 		printf(joystickGetAnalog(1, 2) + ", " + joystickGetAnalog(1, 1));
 		//drive
-		motorSet(2, joystickGetAnalog(1, 2));
-		motorSet(9, -joystickGetAnalog(1, 2));
+		//motorSet(2, joystickGetAnalog(1, 2));
+		//motorSet(9, -joystickGetAnalog(1, 2));
 		//turn
 		motorSet(1, joystickGetAnalog(1, 1));
 	}
