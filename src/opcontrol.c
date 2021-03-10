1d8a5ca5c41494aea938bd8059b63095343c239e
Dirk Wright the 2nd <dirkwrightthe2nd@MacBook-Air-5.local>
2021-03-10 13:17:01 -0800

    test 1

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -3,19 +3,15 @@
 // Mr Wright was here
 //I dont know if im doing this correctly. -Reuben
 //wats up from Nate
-<<<<<<< HEAD
-
-=======
-// yo i'm here - rohun
->>>>>>> bf00621a8d8ef2b511834ad00c0b1a5ac0adacaf
+// do you see this? 
 //This is just a test. You will probably have to change the port numbers.
 void operatorControl() {
 	while (1) {
 		delay(10);
 		printf(joystickGetAnalog(1, 2) + ", " + joystickGetAnalog(1, 1));
 		//drive
-		motorSet(2, joystickGetAnalog(1, 2));
-		motorSet(9, -joystickGetAnalog(1, 2));
+		//motorSet(2, joystickGetAnalog(1, 2));
+		//motorSet(9, -joystickGetAnalog(1, 2));
 		//turn
 		motorSet(1, joystickGetAnalog(1, 1));
 	}
