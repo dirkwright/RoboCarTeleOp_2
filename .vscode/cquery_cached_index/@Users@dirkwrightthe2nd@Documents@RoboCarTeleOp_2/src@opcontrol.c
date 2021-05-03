c252ca6a15597b34f8e334cb8f3b0513cb8d8370
Dirk Wright the 2nd <dirkwrightthe2nd@MacBook-Air-5.local>
2021-05-03 11:55:19 -0700

    Mr Wright

---
+++ .vscode/cquery_cached_index/@Users@dirkwrightthe2nd@Documents@RoboCarTeleOp_2/src@opcontrol.c
@@ -0,0 +1,27 @@
+
+
+#include "main.h"
+#include "auto.h"
+#include "chasis.h"
+
+// Mr Wright was here
+//I dont know if im doing this correctly. -Reuben
+//wats up from Nate
+// Tommy Was Here today
+//cahge by Mr Wright 
+
+//This is just a test. You will probably have to change the port numbers.
+void operatorControl()
+{
+	while (1)
+	{
+		delay(10);
+		//printf("%d, %d" , joystickGetAnalog(1, 2), joystickGetAnalog(1, 1));
+		//drive
+		drive(joystickGetAnalog(1, 2), joystickGetAnalog(1, 1));
+		if (joystickGetDigital(1, 8, JOY_UP))
+		{
+			wallFollow();
+		}
+	}
+}
