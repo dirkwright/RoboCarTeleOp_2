957fa6e488a865c79a0e21c7cee0bead6cfb3ddd
Reuben-L <66336325+Reuben-L@users.noreply.github.com>
2021-05-13 11:32:44 -0700

    added line following and limits to the open loop steering

--- include/auto.h
+++ include/auto.h
@@ -3,15 +3,58 @@
 #include "main.h"
 #include "chasis.h"
 
+int autoSpeed = 50;
+int autoSpeedTurn = 50;
 void wallFollow()
 {
     int ultrasonicFrontValue = ultrasonicGet(ultrasonicFront);
-    
-    if(ultrasonicFrontValue < 20)
+    printf("following a wall, distnce at %d", ultrasonicFrontValue);
+
+    drive(autoSpeed, 0);
+    if (ultrasonicFrontValue < 20 && ultrasonicFrontValue > 1)
     {
-        drive(100, 50);
-        delay(500);
-        drive(100, 0);
+        drive(autoSpeed, autoSpeedTurn);
+        delay(100);
+        drive(autoSpeed, 0);
+        delay(600);
+        drive(autoSpeed, -autoSpeedTurn);
+        delay(100);
+        drive(autoSpeed, 0);
+        delay(100);
+    }
+}
+void lineFollow()
+{
+    int lineRight = analogReadCalibrated(1);
+    int lineLeft = analogReadCalibrated(2);
+    printf("Lineleft: %d and Lineright: %d\n", lineLeft, lineRight);
+    if (lineLeft > 2000)
+    {
+        print("going right");
+        drive(autoSpeed, autoSpeedTurn);
+        delay(100);
+        drive(autoSpeed, 0);
+        delay(700);
+        drive(autoSpeed, -autoSpeedTurn);
+        delay(100);
+        drive(autoSpeed, 0);
+        delay(100);
+    }
+    else if (lineRight > 2000)
+    {
+        print("going left");
+        drive(autoSpeed, -autoSpeedTurn);
+        delay(100);
+        drive(autoSpeed, 0);
+        delay(700);
+        drive(autoSpeed, autoSpeedTurn);
+        delay(100);
+        drive(autoSpeed, 0);
+        delay(100);
+    }
+    else
+    {
+        drive(autoSpeed, 0);
     }
 }
 #endif
